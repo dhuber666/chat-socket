d8f6ddb083d30c49af6d162fb0006d58f129e936
Dominik Huber <dhuber666@gmail.com>
2019-01-13 23:52:02 +0100

    initial commit

---
+++ chat-server/index.js
@@ -0,0 +1,68 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+const cors = require("cors");
+
+let _id = 0;
+const chatRooms = [
+  {
+    name: "general",
+    id: _id++,
+    chats: [
+      {
+        sender: "dominik",
+        message: "hi was geht?"
+      }
+    ],
+    users: []
+  }
+];
+
+app.use(cors());
+
+app.get("/", function(req, res) {
+  res.send("<h1>Hello world</h1>");
+});
+
+app.get("/rooms", (req, res) => {
+  res.send(chatRooms);
+});
+
+io.of("/chat").on("connection", socket => {
+  socket.on("message", (msg, room, name) => {
+    const newChat = {
+      sender: name,
+      message: msg
+    };
+    chatRooms.map(chatRoom => {
+      if (chatRoom.name === room) {
+        chatRoom.chats.push(newChat);
+      }
+    });
+    console.log("new message: ", msg);
+    io.of("/chat")
+      .in(room)
+      .emit("message", msg);
+  });
+  socket.on("joinRoom", (room, name) => {
+    chatRooms.forEach(chatRoom => {
+      console.log(chatRoom);
+      if (chatRoom.users.includes(name.toLowerCase())) {
+        console.log("hello");
+
+        return socket.emit("err", "Username already been taken");
+      } else {
+        chatRoom.users.push(name.toLowerCase());
+        socket.join(room);
+        io.of("/chat")
+          .to(room)
+          .emit("newUser", `${name} has joined the room`);
+        return socket.emit("success", "Joined room ", room);
+      }
+    });
+  });
+});
+
+http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
